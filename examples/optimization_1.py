2594f49822cdc205b00f31f7d397c0c8b9521bb7
AdriSchm92 <adrian.schmidli@thurweb.ch>
2025-03-05 22:42:19 +0100

    Some additional comments

--- examples/optimization_1.py
+++ examples/optimization_1.py
@@ -17,6 +17,8 @@
 # pip install -r requirements.txt
 
 
+# Comment: Adrian Schmidli
+# also run these commands above and than try to execute the marked code with shift + enter!!! 
 
 
 # Standard library imports
